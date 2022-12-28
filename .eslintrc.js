7cb0d4c10b0c0ef3cdc2876ba14468af8dc04be4
cdans <ch.dansard@gmail.com>
2022-12-28 20:39:11 +0100

    add some eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,7 @@ module.exports = {
   root: true,
   extends: "@react-native-community",
   parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
-  quotes: ["error", "double"],
+  plugins: ["@typescript-eslint", "prefer-arrow", "unicorn", "react-native"],
   overrides: [
     {
       files: ["*.ts", "*.tsx"],
